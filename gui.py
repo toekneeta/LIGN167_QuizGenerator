0df72cbc0bb253c0cb49d3d23a9cf915b1f7b3eb
Tony Ta <tony.tta16@gmail.com>
2023-12-04 00:24:32 -0800

    updated design to be centralized

--- gui.py
+++ gui.py
@@ -1,46 +1,68 @@
 import tkinter as tk
 from tkinter import ttk
 
-def submit_answer():
-    # Placeholder function for submitting an answer
-    print("Answer submitted.")
+# Function to generate feedback
+def generate_feedback():
+    user_feedback_text.config(state='normal')
+    user_feedback_text.delete("1.0", tk.END)
+    user_feedback_text.insert(tk.END, "PLACEHOLDER FEEDBACK")
+    user_feedback_text.config(state='disabled')
 
-def request_feedback():
-    # Placeholder function for requesting feedback
-    feedback = "Sample Feedback"
-    return feedback
+# Function to generate progress report
+def generate_progress_report():
+    progress_window = tk.Toplevel()
+    progress_window.title("Progress Report")
+    progress_label = tk.Label(progress_window, text="Progress Report")
+    progress_label.pack()
+    progress_text = tk.Text(progress_window, height=10, width=40)
+    progress_text.pack()
+    progress_text.insert(tk.END, "PLACEHOLDER PROGRESS REPORT")
+    progress_text.config(state=tk.DISABLED)
 
+# Function to generate question
 def generate_question():
-    # Placeholder for actual question generation logic
-    question_text = "New question text here"
-    # app.frames[QuestionPage].generate_question(question_text)
-    return question_text
+    # Clear user feedback
+    user_feedback_text.config(state='normal')
+    user_feedback_text.delete("1.0", tk.END)
+    user_feedback_text.config(state='disabled')
 
-def view_progress_report():
-    # Placeholder function for viewing progress report
-    print("Progress report viewed.")
+    # Clear user answer
+    user_answer_text.delete("1.0", tk.END)
 
-class QuizApp(tk.Tk):
-    def __init__(self):
-        super().__init__()
-        self.title("LIGN 167 Quiz Generator")
+    # Clear old question
+    question_feedback_text.config(state='normal')
+    question_feedback_text.delete("1.0", tk.END)
+    
 
-        self.frames = {}
-        for F in (MainPage, DifficultyPage, QuestionPage, ProgressReportPage):
-            frame = F(self)
-            self.frames[F] = frame
-            frame.grid(row=0, column=0, sticky="nsew")
+    # Generate new question
+    question_feedback_text.insert(tk.END, "PLACEHOLDER QUESTION")
+    question_feedback_text.config(state='disabled')
 
-        self.show_frame(MainPage)
+# Create the main window
+root = tk.Tk()
+root.title("Quiz Generator")
 
-    def show_frame(self, context):
-        frame = self.frames[context]
-        frame.tkraise()
+# Set the size of the window
+root.geometry("800x600")
 
-class MainPage(tk.Frame):
-    def __init__(self, parent):
-        tk.Frame.__init__(self, parent)
-        topics_list = [
+# Create frames for layout
+top_frame = tk.Frame(root)
+top_frame.pack(side=tk.TOP, fill=tk.X)
+
+bottom_frame = tk.Frame(root)
+bottom_frame.pack(side=tk.BOTTOM, fill=tk.X)
+
+right_frame = tk.Frame(root)
+right_frame.pack(side=tk.RIGHT, fill=tk.Y)
+
+left_frame = tk.Frame(root)
+left_frame.pack(side=tk.LEFT, fill=tk.Y)
+
+# Create and place the topic selection widgets
+topic_label = tk.Label(top_frame, text="Select Topic:")
+topic_label.pack(side=tk.TOP, padx=5, pady=5)
+
+topics_list = [
             "Supervised Learning",
             "Linear Regression",
             "Logistic Regression",
@@ -54,134 +76,52 @@ class MainPage(tk.Frame):
             "Autoregressive Language Modeling",
             "Masked Language Modeling",
             "Encoder-decoder Architectures"
-        ]
+]
 
-        # Increase the font size for better visibility
-        large_font = ('Verdana', 20)  # Example font and size
+topic_combobox = ttk.Combobox(top_frame, values=topics_list, state="readonly")
+topic_combobox.pack(side=tk.TOP, padx=5, pady=5)
 
-        topics_label = tk.Label(self, text="Select Topic:", font=large_font)
-        topics_label.pack(pady=10)
+# Create and place the difficulty selection widgets
+difficulty_label = tk.Label(top_frame, text="Select Difficulty:")
+difficulty_label.pack(side=tk.TOP, padx=5, pady=5)
 
-        # Increase the size of the Combobox
-        self.topics = ttk.Combobox(self, values=topics_list, state="readonly", font=large_font)
-        self.topics.pack(pady=10)
+difficulty_list =  ['Easy', 'Medium', 'Hard']
 
-        enter_button = tk.Button(self, text="Enter", command=lambda: parent.show_frame(DifficultyPage), font=large_font)
-        enter_button.pack(pady=20)
+difficulty_combobox = ttk.Combobox(top_frame, values=difficulty_list, state="readonly")
+difficulty_combobox.pack(side=tk.TOP, padx=5, pady=5)
 
-class DifficultyPage(tk.Frame):
-    def __init__(self, parent):
-        tk.Frame.__init__(self, parent)
-        # Add difficulty selection widgets here
-        difficulty_label = tk.Label(self, text="Select Difficulty:")
-        
-        difficulty_label.pack()
-        difficulty = tk.StringVar()
-        tk.Radiobutton(self, text="Easy", variable=difficulty, value="easy").pack()
-        tk.Radiobutton(self, text="Medium", variable=difficulty, value="medium").pack()
-        tk.Radiobutton(self, text="Hard", variable=difficulty, value="hard").pack()
+# Create and place the generate question button
+generate_button = tk.Button(top_frame, text="Generate Question", command=generate_question)
+generate_button.pack(side=tk.TOP, padx=5, pady=5)
 
-        next_button = tk.Button(self, text="Next", command=lambda: parent.show_frame(QuestionPage))
-        next_button.pack(pady=(20, 5))  # Increase space above the "Next" button
+# Create and place the question and feedback text area
+question_feedback_label = tk.Label(left_frame, text="Question:")
+question_feedback_label.pack(side=tk.TOP, padx=5, pady=5)
 
-        back_button = tk.Button(self, text="Back", command=lambda: parent.show_frame(MainPage))
-        back_button.pack(pady=(5, 20))  # Increase space below the "Back" button
+question_feedback_text = tk.Text(left_frame, height=10, width=40, wrap='word', state='disabled')
+question_feedback_text.pack(side=tk.TOP, padx=5, pady=5)
 
-class QuestionPage(tk.Frame):
-    def __init__(self, parent):
-        super().__init__(parent)
-        
-        # Label for the question display
-        question_label = tk.Label(self, text="Question")
-        question_label.pack(pady=(10, 0))
+# Create and place the user answer text area
+user_answer_label = tk.Label(right_frame, text="Your Answer:")
+user_answer_label.pack(side=tk.TOP, padx=5, pady=5)
 
-        # Text area for displaying the question (read-only)
-        self.question_display = tk.Text(self, height=5, width=50, wrap='word', state='disabled')
-        self.question_display.pack(pady=(10, 0))
+user_answer_text = tk.Text(right_frame, height=10, width=40)
+user_answer_text.pack(side=tk.TOP, padx=5, pady=5)
 
-        # Label for the answer input
-        answer_label = tk.Label(self, text="Answer")
-        answer_label.pack(pady=(5, 0))
+# Create and place the feedback text area
+user_feedback_label = tk.Label(right_frame, text="Feedback:")
+user_feedback_label.pack(side=tk.TOP, padx=5, pady=5)
 
-        # Textbox for user input (larger size)
-        self.answer_input = tk.Text(self, height=5, width=50)  # Adjust height as needed
-        self.answer_input.pack(pady=(5, 10))
+user_feedback_text = tk.Text(right_frame, height=10, width=40, wrap='word', state='disabled')
+user_feedback_text.pack(side=tk.TOP, padx=5, pady=5)
 
-         # Submit Answer Button
-        self.submit_button = tk.Button(self, text="Submit Answer", command=self.update_question)
-        self.submit_button.pack()
+# Create and place the submit button
+submit_button = tk.Button(bottom_frame, text="Submit", height=2, width=8, command=generate_feedback)
+submit_button.pack(side=tk.BOTTOM, padx=10, pady=10)
 
+# Create and place the generate progress report button
+progress_report_button = tk.Button(bottom_frame, text="Generate \nProgress Report", height=2, width=15, command=generate_progress_report)
+progress_report_button.pack(side=tk.LEFT, padx=10, pady=10)
 
-        # Feedback label and text box (read-only)
-        feedback_label = tk.Label(self, text="Feedback")
-        feedback_label.pack(pady=(5, 0))
-        self.feedback_display = tk.Text(self, height=5, width=50, wrap='word', state='disabled')
-        self.feedback_display.pack(pady=(0, 10))
-
-        # Preload the first question
-        self.first_question = True
-        self.update_question()
-
-        # New Question Button
-        self.new_question_button = tk.Button(self, text="New Question", command=self.update_question)
-        self.new_question_button.pack()
-
-        progress_button = tk.Button(self, text="Generate Progress Report", command=lambda: parent.show_frame(ProgressReportPage))
-        progress_button.pack(pady=5)
-
-        back_button = tk.Button(self, text="Back", command=lambda: parent.show_frame(DifficultyPage))
-        back_button.pack(pady=5)
-
-    def submit_answer(self):
-        # Placeholder for actual answer submission logic and feedback generation
-        feedback_text = request_feedback()
-        self.update_feedback(feedback_text)
-        self.answer_input.delete(1.0, 'end')  # Clear the answer input
-
-    def generate_new_question(self):
-        # Placeholder for actual new question generation logic
-        self.update_feedback("")  # Clear the feedback
-        self.answer_input.delete(1.0, 'end')  # Clear the answer input
-
-    def update_question(self):
-        # Clear current question and answer
-        self.question_display.config(state='normal')
-        self.question_display.delete(1.0, tk.END)
-
-        # Insert new question
-        new_question_text = generate_question()
-        self.question_display.insert(tk.END, new_question_text)
-        self.question_display.config(state='disabled')
-
-        # Clear the answer input
-        self.answer_input.delete(1.0, tk.END)
-
-        # Update the feedback
-        if not self.first_question:
-            feedback = request_feedback()
-            self.update_feedback(feedback)
-        else:
-            self.first_question = False
-
-    def update_feedback(self, feedback_text):
-        self.feedback_display.config(state='normal')  # Enable editing to update text
-        self.feedback_display.delete(1.0, 'end')  # Clear existing text
-        self.feedback_display.insert('end', feedback_text)  # Insert new feedback text
-        self.feedback_display.config(state='disabled')  # Set back to read-only
-
-class ProgressReportPage(tk.Frame):
-    def __init__(self, parent):
-        tk.Frame.__init__(self, parent)
-
-        progress_report_label = tk.Label(self, text="Progress Report", font=('Verdana', 12))  # Adjust font size as needed
-        progress_report_label.pack(pady=(10, 0))
-
-        self.progress_report_display = tk.Text(self, height=10, width=50, wrap='word', state='disabled')  # Set state to disabled
-        self.progress_report_display.pack(pady=10)
-
-        back_button = tk.Button(self, text="Back", command=lambda: parent.show_frame(QuestionPage))
-        back_button.pack(pady=5)
-
-if __name__ == "__main__":
-    app = QuizApp()
-    app.mainloop()
+# Run the application
+root.mainloop()
