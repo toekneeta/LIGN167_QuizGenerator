1d3f97f67c518123df6b07da7e44b9772c8808a3
Tony Ta <tony.tta16@gmail.com>
2023-12-05 01:55:26 -0800

    added backend

---
+++ backend.py
@@ -0,0 +1,207 @@
+from openai import OpenAI
+
+
+class QuizGenerator:
+	def __init__(self):
+		self.model = "gpt-3.5-turbo-1106"
+		self.client = OpenAI()
+
+		self.topics_list = [
+		            "Supervised Learning",
+		            "Linear Regression",
+		            "Logistic Regression",
+		            "Optimization through Gradient Descent",
+		            "Multilayer Perceptrons",
+		            "Backpropagation",
+		            "Word2Vec",
+		            "Autoregressive Models",
+		            "Attention",
+		            "Transformers",
+		            "Autoregressive Language Modeling",
+		            "Masked Language Modeling",
+		            "Encoder-decoder Architectures"
+		]
+
+		self.difficulty_list =  ['Easy', 'Medium', 'Hard']
+
+		# questions asked for each topic
+		self.question_history = {topic: [] for topic in self.topics_list}
+
+		# counter for number of questions answered for each topic and difficulty
+		self.answered_counter = {topic: {difficulty: 0 for difficulty in self.difficulty_list} for topic in self.topics_list}
+		self.answered = 0
+
+		# counter for number of questions correct for each topic and difficulty
+		self.correct_counter = {topic: {difficulty: 0 for difficulty in self.difficulty_list} for topic in self.topics_list}
+		self.correct = 0
+
+	def generate_question(self, topic, difficulty):
+		generate_question_sys_text = "Your role is to generate quiz questions for a class on \
+	                              deep learning for natural language understanding. \
+	                              I will provide you a topic from the course and a difficulty \
+	                              level (easy, medium, or hard) to guide what kind of question \
+	                              I want you to generate. Questions can be short-answer, fill-in-the-blank, \
+	                              or multiple choice. \
+	                              Your questions should, however, consider \
+	                              adaptive difficulty, meaning that if an user is getting too many \
+	                              questions wrong, you should make questions easier, while if they are \
+	                              getting a lot of questions right, you should make questions harder. \
+	                              After you have generated a question for the user, the user will provide \
+	                              you with their answer to the question. You will give the user feedback to \
+	                              their answer to the question."
+
+		generate_question_user_text = "Generate a " + difficulty + " question in the topic of " + topic + \
+	    							  "Only provide the question in less than 50 words."
+
+	    # if there has been questions for this topic previously, don't repeat them
+		if self.question_history[topic]:
+			generate_question_user_text += " Do not use any variants of the following questions: "
+
+			for question in self.question_history[topic]:
+			    generate_question_user_text += question + " "
+
+
+	    # generate the question
+		gpt_generate_question = self.client.chat.completions.create(
+		    model=self.model,
+		    messages=[
+		        {'role': 'system', "content": generate_question_sys_text},
+		        {'role': 'user', "content": generate_question_user_text}
+	    	]
+	    )
+
+		question = gpt_generate_question.choices[0].message.content
+
+	    # add question to question history for that topic
+		self.question_history[topic].append(question)
+
+		return question
+
+	def provide_feedback(self, question, answer, topic, difficulty):
+		generate_feedback_sys_text = "Your role is to provide feedback for the user \
+	                              on their answer to a question. You will be \
+	                              provided the question and an answer. You will \
+	                              then start your response either with the word \
+	                              correct or incorrect depending on if the user \
+	                              got the question right or wrong. Then, you will \
+	                              give an explanation in less than 50 words why they \
+	                              got the question right or wrong."
+
+		generate_feedback_user_text = 'The question was: "' + \
+		                              question + \
+		                              '" The answer the user gave to the question was "' + \
+		                              answer + '"'
+
+		gpt_generate_feedback = self.client.chat.completions.create(
+		    model=self.model,
+		    messages=[
+		        {'role': 'system', "content": generate_feedback_sys_text},
+		        {'role': 'user', "content": generate_feedback_user_text}
+		    ]
+	    )
+
+		response = gpt_generate_feedback.choices[0].message.content
+		
+		
+		# if answer is correct, update correct_counter
+		if response.split()[0].lower().strip('.,!') == "correct":
+			self.correct_counter[topic][difficulty] += 1
+			self.correct += 1
+
+		# update the answered_counter
+		self.answered_counter[topic][difficulty] += 1
+		self.answered += 1
+
+		return response
+
+	def get_progress_report_stats(self):
+		progress_report_stats = []
+		for topic in self.topics_list:
+			num_easy_answered = self.answered_counter[topic]['Easy']
+			num_medium_answered = self.answered_counter[topic]['Medium']
+			num_hard_answered = self.answered_counter[topic]['Hard']
+
+			num_easy_correct = self.correct_counter[topic]['Easy']
+			num_medium_correct = self.correct_counter[topic]['Medium']
+			num_hard_correct = self.correct_counter[topic]['Hard']
+
+			# percentage of each difficulty of question for that topic answered correctly
+			if num_easy_answered > 0:
+				easy_accuracy_rate = round((num_easy_correct / num_easy_answered) * 100, 2)
+			else:
+				easy_accuracy_rate = -1
+
+			if num_medium_answered > 0:
+				medium_accuracy_rate = round((num_medium_correct / num_medium_answered) * 100, 2)
+			else:
+				medium_accuracy_rate = -1
+
+			if num_hard_answered > 0:
+				hard_accuracy_rate = round((num_hard_correct / num_hard_answered) * 100, 2)
+			else:
+				hard_accuracy_rate = -1
+
+			# overall percentage of correctly answered questions for that topic
+			if num_easy_answered + num_medium_answered + num_hard_answered > 0:
+				topic_accuracy_rate = round(((num_easy_correct + num_medium_correct + num_hard_correct) / \
+									 (num_easy_answered + num_medium_answered + num_hard_answered)) * 100, 2)
+			else:
+				topic_accuracy_rate = -1
+
+			progress_report_stats.append([topic, num_easy_answered, num_medium_answered, num_hard_answered, \
+										  easy_accuracy_rate, medium_accuracy_rate, hard_accuracy_rate, topic_accuracy_rate])
+				
+
+		if self.answered > 0:
+			overall_accuracy_rate = round((self.correct / self.answered) * 100, 2)
+		else:
+			overall_accuracy_rate = -1
+
+		return overall_accuracy_rate, progress_report_stats
+
+	def create_progress_report(self):
+		overall_accuracy_rate, progress_report_stats = self.get_progress_report_stats()
+
+		# user is considered to struggle if topic accuracy rate is less than 70%
+		struggled_topics = []
+		for topic_stats in progress_report_stats:
+			# if user didn't answer question for topic, continue
+			topic = topic_stats[0]
+			topic_accuracy_rate = topic_stats[-1]
+			if topic_accuracy_rate == -1:
+				continue
+			elif topic_accuracy_rate < 70:
+				struggled_topics.append((topic, topic_accuracy_rate))
+
+		# sort struggled topics by accuracy, from least to greatest
+		sorted_struggled_topics = [pair[0] for pair in sorted(struggled_topics, key=lambda x:x[1])]
+
+		generate_study_tips_sys_text = "Your role is to provide study tips for the user. \
+		                                The user will give a list of topics they are struggling \
+		                                with, and your job is to give them advice on how to study \
+		                                for those topics. Keep your response to 50 words maximum \
+		                                for each topic."
+
+		generate_study_tips_user_text = "The following is a list of topics I am struggling with: " + \
+		                                ', '.join(sorted_struggled_topics) + ". Please provide " + \
+		                                "study tips for each topic."
+
+		gpt_generate_study_tips = self.client.chat.completions.create(
+		    model=self.model,
+		    messages=[
+		        {'role': 'system', "content": generate_study_tips_sys_text},
+		        {'role': 'user', "content": generate_study_tips_user_text}
+		    ]
+	    )
+
+		study_tips = gpt_generate_study_tips.choices[0].message.content
+
+		return [overall_accuracy_rate, progress_report_stats, sorted_struggled_topics, study_tips]
+
+
+
+
+
+
+
+
--- gui.py
+++ gui.py
@@ -1,22 +1,79 @@
 import tkinter as tk
 from tkinter import ttk
+from backend import QuizGenerator
 
 # Function to generate feedback
 def generate_feedback():
+    question_feedback_text.config(state='normal')
     user_feedback_text.config(state='normal')
     user_feedback_text.delete("1.0", tk.END)
-    user_feedback_text.insert(tk.END, "PLACEHOLDER FEEDBACK")
-    user_feedback_text.config(state='disabled')
 
+    question = question_feedback_text.get("1.0", tk.END)
+    answer = user_answer_text.get("1.0", tk.END)
+    topic = topic_combobox.get()
+    difficulty = difficulty_combobox.get()
+
+
+    feedback = qg.provide_feedback(question, answer, topic, difficulty)
+    
+    user_feedback_text.insert(tk.END, feedback)
+
+    user_feedback_text.config(state='disabled')
+    question_feedback_text.config(state='disabled')
+
+    
+
+    
 # Function to generate progress report
 def generate_progress_report():
+    overall_accuracy_rate, progress_report_stats, struggled_topics, study_tips = qg.create_progress_report()
+
     progress_window = tk.Toplevel()
+    progress_window.geometry("1600x800")
     progress_window.title("Progress Report")
-    progress_label = tk.Label(progress_window, text="Progress Report")
-    progress_label.pack()
-    progress_text = tk.Text(progress_window, height=10, width=40)
-    progress_text.pack()
-    progress_text.insert(tk.END, "PLACEHOLDER PROGRESS REPORT")
+    
+    headers = ['Topics', '# of Easy Questions Answered', '# of Medium Questions Answered', '# of Hard Questions Answered', \
+               'Easy Accuracy Rate', 'Medium Accuracy Rate', 'Hard Accuracy Rate', 'Topic Accuracy Rate']
+
+    e = tk.Entry(progress_window, width=36, font="Helvetica 8 bold")
+    e.grid(row=0, column=0)
+    e.insert(tk.END, headers[0])
+
+    for i in range(1, 1 + len(topics_list)):
+        e = tk.Entry(progress_window, width=36, font="Helvetica 8")
+        e.grid(row=i, column=0)
+        e.insert(tk.END, progress_report_stats[i-1][0])
+
+    for j in range(1, 8):
+        e = tk.Entry(progress_window, width=32, font="Helvetica 8 bold")
+        e.grid(row=0, column=j)
+        e.insert(tk.END, headers[j])
+
+        for i in range(1, 1 +len(topics_list)):
+            e = tk.Entry(progress_window, width=32, font="Helvetica 8")
+            e.grid(row=i, column=j)
+            if progress_report_stats[i-1][j] == -1:
+                e.insert(tk.END, "")
+            elif j < 4:
+                e.insert(tk.END, progress_report_stats[i-1][j])
+            else:
+                e.insert(tk.END, str(progress_report_stats[i-1][j]) + "%")
+
+
+    progress_text = tk.Text(progress_window, height=30)
+    progress_text.grid(row=len(topics_list) + 2, column=0, columnspan=8)
+
+    if overall_accuracy_rate == -1:
+        progress_str = "Total accuracy rate across topics: \n\n"
+    else:
+        progress_str = "Total accuracy rate across topics: " + str(overall_accuracy_rate) + "%\n\n"
+    progress_str += "Topics to refresh: " + ', '.join(struggled_topics) + '\n\n'
+    progress_str += "Tips on studying:\n"
+
+    if struggled_topics:
+        progress_str += study_tips
+
+    progress_text.insert(tk.END, progress_str)
     progress_text.config(state=tk.DISABLED)
 
 # Function to generate question
@@ -33,98 +90,15 @@ def generate_question():
     question_feedback_text.config(state='normal')
     question_feedback_text.delete("1.0", tk.END)
     
+    # get topic and difficulty
+    topic = topic_combobox.get()
+    difficulty = difficulty_combobox.get()
 
     # Generate new question
-    question_feedback_text.insert(tk.END, "PLACEHOLDER QUESTION")
+    question = qg.generate_question(topic, difficulty)
+
+    question_feedback_text.insert(tk.END, question)
     question_feedback_text.config(state='disabled')
-
-# Create the main window
-root = tk.Tk()
-root.title("Quiz Generator")
-
-# Set the size of the window
-root.geometry("800x600")
-
-# Create frames for layout
-top_frame = tk.Frame(root)
-top_frame.pack(side=tk.TOP, fill=tk.X)
-
-bottom_frame = tk.Frame(root)
-bottom_frame.pack(side=tk.BOTTOM, fill=tk.X)
-
-right_frame = tk.Frame(root)
-right_frame.pack(side=tk.RIGHT, fill=tk.Y)
-
-left_frame = tk.Frame(root)
-left_frame.pack(side=tk.LEFT, fill=tk.Y)
-
-# Create and place the topic selection widgets
-topic_label = tk.Label(top_frame, text="Select Topic:")
-topic_label.pack(side=tk.TOP, padx=5, pady=5)
-
-topics_list = [
-            "Supervised Learning",
-            "Linear Regression",
-            "Logistic Regression",
-            "Optimization through Gradient Descent",
-            "Multilayer Perceptrons",
-            "Backpropagation",
-            "Word2Vec",
-            "Autoregressive Models",
-            "Attention",
-            "Transformers",
-            "Autoregressive Language Modeling",
-            "Masked Language Modeling",
-            "Encoder-decoder Architectures"
-]
-
-topic_combobox = ttk.Combobox(top_frame, values=topics_list, state="readonly")
-topic_combobox.current(0)
-topic_combobox.pack(side=tk.TOP, padx=5, pady=5)
-
-# Create and place the difficulty selection widgets
-difficulty_label = tk.Label(top_frame, text="Select Difficulty:")
-difficulty_label.pack(side=tk.TOP, padx=5, pady=5)
-
-difficulty_list =  ['Easy', 'Medium', 'Hard']
-
-difficulty_combobox = ttk.Combobox(top_frame, values=difficulty_list, state="readonly")
-difficulty_combobox.current(0)
-difficulty_combobox.pack(side=tk.TOP, padx=5, pady=5)
-
-# Create and place the generate question button
-generate_button = tk.Button(top_frame, text="Generate Question", command=generate_question)
-generate_button.pack(side=tk.TOP, padx=5, pady=5)
-
-# Create and place the question and feedback text area
-question_feedback_label = tk.Label(left_frame, text="Question:")
-question_feedback_label.pack(side=tk.TOP, padx=5, pady=5)
-
-question_feedback_text = tk.Text(left_frame, height=10, width=40, wrap='word', state='disabled')
-question_feedback_text.pack(side=tk.TOP, padx=5, pady=5)
-
-# Create and place the user answer text area
-user_answer_label = tk.Label(right_frame, text="Your Answer:")
-user_answer_label.pack(side=tk.TOP, padx=5, pady=5)
-
-user_answer_text = tk.Text(right_frame, height=10, width=40)
-user_answer_text.pack(side=tk.TOP, padx=5, pady=5)
-
-# Create and place the feedback text area
-user_feedback_label = tk.Label(right_frame, text="Feedback:")
-user_feedback_label.pack(side=tk.TOP, padx=5, pady=5)
-
-user_feedback_text = tk.Text(right_frame, height=10, width=40, wrap='word', state='disabled')
-user_feedback_text.pack(side=tk.TOP, padx=5, pady=5)
-
-# Create and place the submit button
-submit_button = tk.Button(bottom_frame, text="Submit", height=2, width=8, command=generate_feedback)
-submit_button.pack(side=tk.BOTTOM, padx=10, pady=10)
-
-# Create and place the generate progress report button
-progress_report_button = tk.Button(bottom_frame, text="Generate \nProgress Report", height=2, width=15, command=generate_progress_report)
-progress_report_button.pack(side=tk.LEFT, padx=10, pady=10)
-
 
 def open_help():
     # Create a new top-level window
@@ -183,11 +157,110 @@ def open_help():
 
     guide_text.configure(state="disabled")
     guide_text.pack(anchor='w')
-        
+
+qg = QuizGenerator()
+
+# Data Structures
+topics_list = [
+            "Supervised Learning",
+            "Linear Regression",
+            "Logistic Regression",
+            "Optimization through Gradient Descent",
+            "Multilayer Perceptrons",
+            "Backpropagation",
+            "Word2Vec",
+            "Autoregressive Models",
+            "Attention",
+            "Transformers",
+            "Autoregressive Language Modeling",
+            "Masked Language Modeling",
+            "Encoder-decoder Architectures"
+]
+
+
+difficulty_list =  ['Easy', 'Medium', 'Hard']
+
+# Create the main window
+root = tk.Tk()
+root.title("Quiz Generator")
+
+# Set the size of the window
+root.geometry("800x600")
+
+# Create frames for layout
+top_frame = tk.Frame(root)
+top_frame.pack(side=tk.TOP, fill=tk.X)
+
+bottom_frame = tk.Frame(root)
+bottom_frame.pack(side=tk.BOTTOM, fill=tk.X)
+
+right_frame = tk.Frame(root)
+right_frame.pack(side=tk.RIGHT, fill=tk.Y)
+
+left_frame = tk.Frame(root)
+left_frame.pack(side=tk.LEFT, fill=tk.Y)
+
+# Create and place the topic selection widgets
+topic_label = tk.Label(top_frame, text="Select Topic:")
+topic_label.pack(side=tk.TOP, padx=5, pady=5)
+
+topic_combobox = ttk.Combobox(top_frame, values=topics_list, state="readonly")
+topic_combobox.current(0)
+topic_combobox.pack(side=tk.TOP, padx=5, pady=5)
+
+
+# Create and place the difficulty selection widgets
+difficulty_label = tk.Label(top_frame, text="Select Difficulty:")
+difficulty_label.pack(side=tk.TOP, padx=5, pady=5)
+
+difficulty_combobox = ttk.Combobox(top_frame, values=difficulty_list, state="readonly")
+difficulty_combobox.current(0)
+difficulty_combobox.pack(side=tk.TOP, padx=5, pady=5)
+
+
+# Create and place the generate question button
+generate_button = tk.Button(top_frame, text="Generate Question", command=generate_question)
+generate_button.pack(side=tk.TOP, padx=5, pady=5)
+
+
+# Create and place the question and feedback text area
+question_feedback_label = tk.Label(left_frame, text="Question:")
+question_feedback_label.pack(side=tk.TOP, padx=5, pady=5)
+
+question_feedback_text = tk.Text(left_frame, height=10, width=40, wrap='word', state='disabled')
+question_feedback_text.pack(side=tk.TOP, padx=5, pady=5)
+
+
+# Create and place the user answer text area
+user_answer_label = tk.Label(right_frame, text="Your Answer:")
+user_answer_label.pack(side=tk.TOP, padx=5, pady=5)
+
+user_answer_text = tk.Text(right_frame, height=10, width=40)
+user_answer_text.pack(side=tk.TOP, padx=5, pady=5)
+
+
+# Create and place the feedback text area
+user_feedback_label = tk.Label(right_frame, text="Feedback:")
+user_feedback_label.pack(side=tk.TOP, padx=5, pady=5)
+
+user_feedback_text = tk.Text(right_frame, height=10, width=40, wrap='word', state='disabled')
+user_feedback_text.pack(side=tk.TOP, padx=5, pady=5)
+
+
+# Create and place the submit button
+submit_button = tk.Button(bottom_frame, text="Submit", height=2, width=8, command=generate_feedback)
+submit_button.pack(side=tk.BOTTOM, padx=10, pady=10)
+
+
+# Create and place the generate progress report button
+progress_report_button = tk.Button(bottom_frame, text="Generate \nProgress Report", height=2, width=15, command=generate_progress_report)
+progress_report_button.pack(side=tk.LEFT, padx=10, pady=10)
+
 
 # Add Help button
 help_button = tk.Button(bottom_frame, text="Help", command=open_help)
 help_button.pack(side=tk.LEFT, padx=10, pady=10)
+
 
 
 # Run the application
